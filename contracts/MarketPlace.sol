d766f353d5f8f63532c56d8069913ac535b1dfbb
"Sirbu Alexandru-Gabriel" <"sirbualexandrugabriel@gmail.com">
2022-04-30 23:04:20 +0300

    optimized space used by not storing redundant information of products, refactored code and added a function to get all products of a user and to check ownership of a product

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -26,14 +26,14 @@ contract MarketPlace is Ownable {
         uint256 price;
         uint256 fees;
         uint256 datetime;
-        Product product;
+        uint256 productId;
     }
 
     uint256 private idCount = 0;
 
     mapping(address => mapping(uint256 => SellableProduct)) public ownedSellableProducts;
 
-    mapping(address => mapping(uint256 => Product)) private ownedNonSellableProducts;
+    mapping(address => mapping(uint256 => bool)) private ownedNonSellableProducts;
 
     mapping(uint256 => TransactionDetails[]) public productHistory;
 
@@ -41,67 +41,68 @@ contract MarketPlace is Ownable {
 
     uint256 private feePricePercentage = 2;
 
+    modifier isProductOwner(uint256 _productId, address _owner) {
+        require(products[_productId].owner == _owner, "The supplied address is not the owner of the product!");
+        _;
+    }
+
     function setFeePercentage(uint256 _feePercentage) external onlyOwner {
         feePricePercentage = _feePercentage;
     }
 
-    function clone(Product memory product) internal pure returns (Product memory) {
-        return Product(product.id, product.owner, product.name, product.description, product.link, product.tags, product.forSale);
+    function clone(Product memory _product) internal pure returns (Product memory) {
+        return Product(_product.id, _product.owner, _product.name, _product.description, _product.link, _product.tags, _product.forSale);
     }
 
     function listProductForSale(string memory _name, string memory _description, string memory _link, string[] memory _tags, uint256 _price) external {
         uint256 id = idCount++;
         Product memory product = Product(id, msg.sender, _name, _description, _link, _tags, true);
         products.push(product);
-        ownedSellableProducts[msg.sender][id] = SellableProduct(_price, _price * feePricePercentage / 100, block.timestamp, product);
+        ownedSellableProducts[msg.sender][id] = SellableProduct(_price, _price * feePricePercentage / 100, block.timestamp, id);
     }
 
     function addNonSellableProduct(string memory _name, string memory _description, string memory _link, string[] memory _tags) external {
         uint256 id = idCount++;
         Product memory product = Product(id, msg.sender, _name, _description, _link, _tags, false);
         products.push(product);
-        ownedNonSellableProducts[msg.sender][id] = product;
+        ownedNonSellableProducts[msg.sender][id] = true;
     }
 
     function transferFromSellableToNonSellable(address _ownerSellable, address _ownerNonSellable, uint256 _productId) internal {
-        ownedNonSellableProducts[_ownerNonSellable][_productId] = ownedSellableProducts[_ownerSellable][_productId].product;
-        ownedNonSellableProducts[_ownerNonSellable][_productId].owner = _ownerNonSellable;
-        ownedNonSellableProducts[_ownerNonSellable][_productId].forSale = false;
+        ownedNonSellableProducts[_ownerNonSellable][_productId] = true;
         products[_productId].owner = _ownerNonSellable;
         products[_productId].forSale = false;
         delete(ownedSellableProducts[_ownerSellable][_productId]);
     }
 
-    function transferFromNonSellableToSellable(address _owner, uint256 _productId, uint256 price) internal {
-        ownedSellableProducts[_owner][_productId] = SellableProduct(price, price * feePricePercentage / 100, block.timestamp, ownedNonSellableProducts[_owner][_productId]);
-        ownedSellableProducts[_owner][_productId].product.forSale = true;
+    function transferFromNonSellableToSellable(address _owner, uint256 _productId, uint256 _price) internal {
+        ownedSellableProducts[_owner][_productId] = SellableProduct(_price, _price * feePricePercentage / 100, block.timestamp, _productId);
         products[_productId].forSale = true;
         delete(ownedNonSellableProducts[_owner][_productId]);
     }
 
-    function listExistingProductForSale(uint256 _productId, uint256 _price) external {
-        require(ownedNonSellableProducts[msg.sender][_productId].owner == msg.sender, "Sender is not the owner of the product!");
+    function listExistingProductForSale(uint256 _productId, uint256 _price) external isProductOwner(_productId, msg.sender) {
         transferFromNonSellableToSellable(msg.sender, _productId, _price);
     }
     
-    function buyProduct(uint256 productId, address productSeller) external payable {
-        SellableProduct memory soldProduct = ownedSellableProducts[productSeller][productId];
-        require(soldProduct.product.owner == productSeller && msg.value >= soldProduct.price + soldProduct.fees, "Not enough wei sent!");
-        (bool success, ) = payable(productSeller).call{value: soldProduct.price}("");
+    function buyProduct(uint256 _productId, address _productSeller) external payable isProductOwner(_productId, _productSeller) {
+        SellableProduct memory soldProduct = ownedSellableProducts[_productSeller][_productId];
+        require(msg.value >= soldProduct.price + soldProduct.fees, "Not enough wei sent!");
+        (bool success, ) = payable(_productSeller).call{value: soldProduct.price}("");
         require(success, "Transaction failed!");
         
-        productHistory[productId].push(TransactionDetails(soldProduct.price, soldProduct.fees, soldProduct.datetime, productSeller, msg.sender));
-        transferFromSellableToNonSellable(productSeller, msg.sender, productId);
+        productHistory[_productId].push(TransactionDetails(soldProduct.price, soldProduct.fees, soldProduct.datetime, _productSeller, msg.sender));
+        transferFromSellableToNonSellable(_productSeller, msg.sender, _productId);
 
         (success, ) = payable(owner()).call{value: soldProduct.fees}("");
         require(success, "Fees payment failed but transaction was successful!");
     }
 
-    function getAllProductsForSale(uint256 startIndexForSearch, uint256 maxResults) external view returns (uint256, Product[] memory) {
+    function getAllProductsForSale(uint256 _startIndexForSearch, uint256 _maxResults) external view returns (uint256, Product[] memory) {
         Product[] memory productsForSale = new Product[](products.length);
         uint productsForSaleCounter = 0;
         uint i;
-        for (i = startIndexForSearch; i < products.length && productsForSaleCounter < maxResults; i++) {
+        for (i = _startIndexForSearch; i < products.length && productsForSaleCounter < _maxResults; i++) {
             if (products[i].forSale) {
                 productsForSale[productsForSaleCounter++] = products[i];
             }
@@ -109,16 +110,16 @@ contract MarketPlace is Ownable {
         return (i, productsForSale);
     }
 
-    function filterSellingProducts(string memory partialTitle, string memory category, address owner, uint256 date, uint256 startIndexForSearch, uint256 maxResults) external view returns (uint256, Product[] memory) {
+    function filterSellingProducts(string memory _partialTitle, string memory _category, address _owner, uint256 _date, uint256 _startIndexForSearch, uint256 _maxResults) external view returns (uint256, Product[] memory) {
         Product[] memory productsForSale = new Product[](products.length);
         uint productsForSaleCounter = 0;
         uint i;
-        for (i = startIndexForSearch; i < products.length && productsForSaleCounter < maxResults; i++) {
+        for (i = _startIndexForSearch; i < products.length && productsForSaleCounter < _maxResults; i++) {
             if (!products[i].forSale) {
                 continue;
             }
             bool isValid = true;
-            bytes32 categoryHash = keccak256(abi.encodePacked(category));
+            bytes32 categoryHash = keccak256(abi.encodePacked(_category));
             if (categoryHash != keccak256(abi.encodePacked(""))) {
                 string[] memory tags = products[i].tags;
                 uint j;
@@ -132,9 +133,9 @@ contract MarketPlace is Ownable {
                 }
             }
             if (!(isValid &&
-                (owner != address(0) || owner == products[i].owner) &&
-                (date != 0 || date <= ownedSellableProducts[products[i].owner][i].datetime) &&
-                containsWord(partialTitle, products[i].name)    // don't need to check for empty string since it's included into the product name
+                (_owner != address(0) || _owner == products[i].owner) &&
+                (_date != 0 || _date <= ownedSellableProducts[products[i].owner][i].datetime) &&
+                containsWord(_partialTitle, products[i].name)    // don't need to check for empty string since it's included into the product name
             )) {
                 isValid = false;
             }
@@ -145,19 +146,19 @@ contract MarketPlace is Ownable {
         return (i, productsForSale);
     }
 
-    function getHistoryForProduct(uint256 productId) external view returns (TransactionDetails[] memory) {
-        return productHistory[productId];
+    function getHistoryForProduct(uint256 _productId) external view returns (TransactionDetails[] memory) {
+        return productHistory[_productId];
     }
 
-    function getSellableProductDetails(uint256 productId) external view returns (SellableProduct memory) {
-        Product memory product = products[productId];
+    function getSellableProductDetails(uint256 _productId) external view returns (SellableProduct memory) {
+        Product memory product = products[_productId];
         require(product.forSale == true);
-        return ownedSellableProducts[product.owner][productId];
+        return ownedSellableProducts[product.owner][_productId];
     }
 
-    function containsWord(string memory what, string memory where) internal pure returns (bool found) {
-        bytes memory whatBytes = bytes (what);
-        bytes memory whereBytes = bytes (where);
+    function containsWord(string memory _what, string memory _where) internal pure returns (bool found) {
+        bytes memory whatBytes = bytes(_what);
+        bytes memory whereBytes = bytes(_where);
         if (whereBytes.length < whatBytes.length) { 
             return false; 
         }
@@ -176,5 +177,22 @@ contract MarketPlace is Ownable {
             }
         }
         return found;
+    }
+
+    function getOwnerProducts(uint256 _startIndexForSearch, uint256 _maxResults) external view returns (uint256, Product[] memory) {
+        address owner = msg.sender;
+        Product[] memory ownerProducts = new Product[](products.length);
+        uint256 ownerProductsCounter = 0;
+        uint256 i;
+        for (i = _startIndexForSearch; i < products.length && ownerProductsCounter < _maxResults; i++) {
+            if (products[i].owner == owner) {
+                ownerProducts[ownerProductsCounter++] = products[i];
+            }
+        }
+        return (i, ownerProducts);
+    }
+
+    function checkProductOwnership(uint256 _productId) external view returns (address) {
+        return products[_productId].owner;
     }
 }
